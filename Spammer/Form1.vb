17269592b97ce48b98ee8102ab7f663a98a28f61
Jason Briggs <johnfrayster@gmail.com>
2015-12-07 16:30:39 -0500

    TextSpammer 2008

---
+++ Spammer/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Spam.Interval = Interval.Text
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Timer1.Enabled = False
+        Spam.Enabled = False
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Spam_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Spam.Tick
+        SendKeys.Send(Interval.Text)
+    End Sub
+End Class
